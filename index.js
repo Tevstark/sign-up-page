08dcac1e38b9e9a2c2ee968573c9ce983d03c464
TevStark <tevkamau@gmail.com>
2024-03-04 23:50:28 +0300

    Trying JS Validation

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-document.addEventListener("DOMContentLoaded", function () {
-  let password = document.querySelector("#password");
-  let confirmPassword = document.querySelector("#confirm-password");
-  let btn = document.querySelector("#submit-form");
 
-  btn.addEventListener("click", () => {
-    if (password.value === confirmPassword.value) {
-      alert("Passwords match.");
-    } else {
-      alert("Passwords don't match.");
+document.querySelector('.signup').addEventListener('invalid', (event) => {
+    if (event.target.validity.valueMissing) {
+      event.target.nextElementSibling.textContent = 'This field is required.';
+      event.target.style.border = "red";
+    } else if (event.target.validity.typeMismatch) {
+      switch (event.target.type) {
+        case 'email':
+          event.target.nextElementSibling.textContent = 'Please enter a valid email address.';
+          break;
+        case 'tel':
+          event.target.nextElementSibling.textContent = 'Please enter a valid phone number in the format XXX-XXX-XXXX.';
+          break;
+        case 'password':
+          event.target.nextElementSibling.textContent = 'Please enter a valid password.';
+          event.target.style.bordercolor = "red";
+          break;
+        default:
+          break;
+      }
     }
-  });
-});
+  }, true);
